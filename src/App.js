5fc4265b15cfb0f730b21d8c4279aaede6435bce
obaida <obaidashurbaji99@gmail.com>
2022-09-13 18:49:14 +0200

    start with react styling some stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-	const title = 'Welcome to the new blog';
-	const likes = 50;
+	// const title = 'Welcome to the new blog';
+	// const likes = 50;
 	// const person = {
 	// 	name: 'Obaida',
 	// 	age: 30,
 	// };
 	// const bool = true;
-	const link = 'https://www.google.com';
+	// const link = 'https://www.google.com';
 
 	return (
 		<div className="App">
@@ -18,18 +17,20 @@ function App() {
 
 			<div className="content">
 				<Home />
-				<h1>{title}</h1>
-				<p>Liked {likes} times</p>
+
+
+				{/* <h1>{title}</h1>
+				<p>Liked {likes} times</p> */}
 
 				{/* <p>{person}</p> */}
 				{/* <p>{bool}</p> */}
 
-				<p>{10}</p>
+				{/* <p>{10}</p>
 				<p>{'hello ninjas'}</p>
 				<p>{[1, 2, 3, 4, 5, 6]}</p>
 				<p>{Math.floor(Math.random() * 10)}</p>
 
-				<a href={link}>Google</a>
+				<a href={link}>Google</a> */}
 			</div>
 		</div>
 	);

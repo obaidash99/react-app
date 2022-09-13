943e4e3e946477dcd11bee541a257e0990fe68e0
obaida <obaidashurbaji99@gmail.com>
2022-09-13 19:47:52 +0200

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+	const handleClick = (e) => {
+		console.log('hello ninjaas', e);
+	};
+
+	const handleClickAgain = (name, e) => {
+		console.log('Hello ' + name, e.target);
+	};
+
 	return (
 		<div className="home">
 			<h2>HomePage</h2>
+			<button onClick={handleClick}>Click Me</button>
+
+			<button onClick={(e) => handleClickAgain('Obaida', e)}>Click Me Again!</button>
 		</div>
 	);
 };
